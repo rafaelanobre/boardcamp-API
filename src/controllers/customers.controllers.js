93c4cee50263916c7b59857d4e337d13fa144d91
rafaelanobre <raafanobre@gmail.com>
2023-07-29 18:56:15 -0300

    :sparkles: Feat: customer get by id and update

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -10,13 +10,26 @@ export async function listCustomers (req, res){
     }
 }
 
+export async function listById(req, res){
+    const {id} = req.params;
+    try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
+        if(customer.rowCount===0){
+            return res.status(404).send("Usuário não encontrado");
+        }
+        res.status(200).send(customer.rows[0]);
+    } catch(err){
+        res.status(500).send(err.message);
+    }
+}
+
 export async function addCustomers (req, res){
     const {name, phone, cpf, birthday} = req.body; 
 
     try{
         const customersAlredyExists = await db.query(`SELECT * FROM customers WHERE cpf=$1;`, [cpf]);
 
-        if(customersAlredyExists.rows.length > 0){
+        if(customersAlredyExists.rowCount > 0){
             return res.status(409).send('Esse cliente já está cadastrado.');
         }
 
@@ -27,6 +40,29 @@ export async function addCustomers (req, res){
 
         res.sendStatus(201);
     }catch(err){
+        res.status(500).send(err.message);
+    }
+}
+
+export async function updateCustomer(req, res){
+    const {id} = req.params;
+    const {name, phone, cpf, birthday} = req.body; 
+
+    try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
+        if(customer.rowCount===0){
+            return res.status(404).send("Usuário não encontrado");
+        }
+
+        const customersAlredyExists = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`, [cpf, id]);
+
+        if(customersAlredyExists.rowCount > 0){
+            return res.status(409).send('Esse CPF já está cadastrado em outro cliente.');
+        }
+
+        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`,[name, phone, cpf, birthday, id]);
+        res.sendStatus(200);
+    } catch(err){
         res.status(500).send(err.message);
     }
 }
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { addCustomers, listCustomers } from "../controllers/customers.controllers.js";
+import { addCustomers, listById, listCustomers, updateCustomer } from "../controllers/customers.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { customerSchema } from "../schemas/customerSchema.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers", listCustomers);
-customersRouter.get("/customers/:id", )
+customersRouter.get("/customers/:id", listById)
 customersRouter.post("/customers",validateSchema(customerSchema), addCustomers)
-customersRouter.put("/customers/:id", )
+customersRouter.put("/customers/:id", validateSchema(customerSchema), updateCustomer)
 
 export default customersRouter;
