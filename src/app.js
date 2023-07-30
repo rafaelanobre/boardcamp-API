6dacc61b254f93b3eef93f2ceb5bd9049ab2f061
rafaelanobre <raafanobre@gmail.com>
2023-07-30 19:28:12 -0300

    :rocket: Deploy config

--- src/app.js
+++ src/app.js
@@ -8,5 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = 5000;
-app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`);
+});
